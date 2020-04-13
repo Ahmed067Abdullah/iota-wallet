13829d971c65cc2b6c1b798cd270605f0b7dac32
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-13 11:02:45 +0500

    iota config changed

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
-export const defaultSeed1 =
-  "RTJCKLYRPFQH9OYS9RDZCWBR9KXLUGYWZSRZLLMCWXPU9HUSBRWVVTVFZKMQXO99YATFIEZVGZTK9CBB9"; // most tx and balance
 export const defaultSeed =
-  "ATJCKLYRPFQH9OYS9RDZCWBR9KXLUGYWZSRZLLMCWXPU9HUSBRWVVTVFZKMQXO99YATFIEZVGZTK9CBBA";
+  "RTJCKLYRPFQH9OYS9RDZCWBR9KXLUGYWZSRZLLMCWXPU9HUSBRWVVTVFZKMQXO99YATFIEZVGZTK9CBB9"; // most tx and balance
+// export const defaultSeed =
+//   "ATJCKLYRPFQH9OYS9RDZCWBR9KXLUGYWZSRZLLMCWXPU9HUSBRWVVTVFZKMQXO99YATFIEZVGZTK9CBBA";
 
-export const testNodeURL = "https://nodes.devnet.iota.org:443";
+// export const testNodeURL = "https://nodes.devnet.iota.org:443";
+export const testNodeURL = "https://nodes.comnet.thetangle.org:443";
 
 export const depth = 3;
 
-export const minWeightMagnitude = 14;
+export const minWeightMagnitude = 10;
